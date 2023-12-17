997e812b4c8a7437e36050bb282287ad7370cbb8
MartKon <martkon77@gmail.com>
2023-12-17 03:22:19 +0200

    Day 12
    Connected the arms and body, made a level, upgraded the menus.

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -15,8 +15,9 @@ public class GameScript : MonoBehaviour
     public Transform MouseTracker;
     public GameObject HandR;
     public GameObject HandL;
-    public GameObject ClimbWall;
+    [SerializeField] GameObject ClimbWall;
     SpriteRenderer[] BGArray;
+    public GameObject WinHold;
 
     [Header("Hand properties")]
     [Range(0f, 1f)]
@@ -39,6 +40,14 @@ public class GameScript : MonoBehaviour
         BGArray = ClimbWall.GetComponentsInChildren<SpriteRenderer>();
         foreach(SpriteRenderer x in BGArray) { x.color = new Color(Random.Range(212f, 222f) / 255, Random.Range(124f, 134f) / 255, Random.Range(68f, 78f) / 255); }
     }
+    void Update()
+    {
+        if ((Vector3.Distance(HandR.transform.position, WinHold.transform.position) <= 0.5f && HandR.GetComponent<HandScript>().onHold) ||
+           (Vector3.Distance(HandL.transform.position, WinHold.transform.position) <= 0.5f && HandL.GetComponent<HandScript>().onHold))
+        {
+            isWin = true;
+        }
+    }
     public void LoadLevel1()
     {
         SceneManager.LoadScene(1);
@@ -50,5 +59,13 @@ public class GameScript : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
     }
 }
